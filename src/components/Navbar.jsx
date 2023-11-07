1ed6731ee2a462da1ec89e10207da19bde6c2eb3
vaibhavraj-24 <142780674+vaibhavraj-24@users.noreply.github.com>
2023-11-07 11:06:26 +0530

    real changes

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,14 +1,15 @@
-import React, { useState } from 'react'
-import { Link, json } from 'react-router-dom'
+import React from 'react'
+import { Link } from 'react-router-dom'
 
 
 const Navbar = () => {
 
-   const [filterText, setFilterText] = useState('');
+  // const [filterText, setFilterText] = useState('');
 
-  const handleFilterTextChange = (newFilterText) => {
-    setFilterText(newFilterText);
-  };
+  // const handleFilterTextChange = (newFilterText) => {
+  //   setFilterText(newFilterText);
+  // };
+
 
 
   return (
